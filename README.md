1debeb38fa04e793b63524148b9265fa5132e5a2
Rhys Tyers <5313660+rhyst@users.noreply.github.com>
2023-02-24 08:13:50 +0000

    Use edits, redactions, and html content to make the message interface nicer

--- README.md
+++ README.md
@@ -1,17 +1,58 @@
 # WeatherBot
 
-A simple [maubot](https://github.com/maubot/maubot) that scrapes windy.com for a multi-model forecast given a set of coordinates.
+A simple [maubot](https://github.com/maubot/maubot) that scrapes windy.com for a multi-model forecast given a set of coordinates. Docs are [here](https://docs.mau.fi/maubot/index.html).
+
+## Installation
+
+You will need `maubot` running on your matrix server. The instructions for running this manually are [here](https://docs.mau.fi/maubot/usage/setup/index.html) but you may be using something else to manage your matrix instance (like [matrix-docker-ansible-deploy](https://github.com/spantaleev/matrix-docker-ansible-deploy/)) in which case refer to that.
+
+This bot requires `pyppeteer` to be installed. See [rhyst/maubot](https://github.com/rhyst/maubot-weather) for a modified `maubot` image that you can use if you are running via docker.
+
+Once you have `maubot` running you can visit it's web management page to install `maubot-weather`. Follow the instructions [here](https://docs.mau.fi/maubot/usage/basic.html) to do this. You can download the plugin from the [releases page](https://github.com/rhyst/maubot-weather/releases).
 
 ## Usage
 
 Send the following messages to receive a reply from the bot:
 
+- Share a location with the bot for the forecast for that location
 - `<lat> <lon>` - Reply with the forecast for the given lat lon
 - `version` - Reply with the bot version
-- Share a location with the bot for the forecast for that location
+- `auth` - Check windy authentication status
 
 Any other message will cause the bot to reply with help text.
 
-## Requirements
 
-A `maubot` docker image or other environment with `pyppeteer` installed. See [rhyst/maubot](https://github.com/rhyst/maubot-weather).
+## Development
+
+Run:
+
+```
+poetry install
+```
+
+To produce a build run:
+
+```
+poetry run mbc build
+```
+
+You can also deploy directly to the maubot instance. First authenticate the maubot instance:
+
+```
+poetry run mbc login -u maubot_user -p 'maubot_password' -s https://matrix.example.com -a my_server_alias
+```
+
+Then you can build and upload directly with this:
+
+```
+poetry run mbc build -u
+```
+
+To create a github release tag the version and use the `gh` cli tool:
+
+```
+git tag x.x.x
+git push --tags
+gh release --latest x.x.x xyz.maubot.weather-vx.x.x.mbp
+```
+
--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import asyncio
 import re
+from typing import Tuple
 from pyppeteer import launch
-from mautrix.types import EventType, MessageType, MediaMessageEventContent, ImageInfo
+from mautrix.types import (
+    EventType,
+    RelationType,
+    RelatesTo,
+    TextMessageEventContent,
+    MessageType,
+    MediaMessageEventContent,
+    ImageInfo,
+    MessageEvent,
+    Format,
+)
 from maubot import Plugin
 from maubot.handlers import event, web
 from aiohttp.web import Request, Response
@@ -12,24 +23,49 @@ class WeatherBot(Plugin):
     cookie_account_ss = None
     cookie_account_sid = None
 
+    async def send_message(self, room_id: str, message: str):
+        return await self.client.send_message(
+            room_id,
+            TextMessageEventContent(
+                body=message,
+                formatted_body=message,
+                msgtype=MessageType.TEXT,
+                format=Format.HTML,
+            ),
+        )
+
+    async def edit_message(self, room_id: str, message_id: str, message: str):
+        return await self.client.send_message_event(
+            room_id,
+            EventType.ROOM_MESSAGE,
+            TextMessageEventContent(
+                body=message,
+                msgtype=MessageType.TEXT,
+                relates_to=RelatesTo(
+                    rel_type=RelationType.REPLACE,
+                    event_id=message_id,
+                ),
+            ),
+        )
+
     @web.get("/login")
     async def login(self, req: Request) -> Response:
         return Response(
             text="""
-        <form action="" method="post">
-            <div>
-                <label for="email">Email: </label>
-                <input name="email" />
-            </div>
-            <div>
-                <label for="password">Password: </label>
-                <input type="password" name="password">
-            </div>
-            <div>
-                <input type="submit" value="Login">
-            </div>
-        </form>
-        """,
+                <form action="" method="post">
+                    <div>
+                        <label for="email">Email: </label>
+                        <input name="email" />
+                    </div>
+                    <div>
+                        <label for="password">Password: </label>
+                        <input type="password" name="password">
+                    </div>
+                    <div>
+                        <input type="submit" value="Login">
+                    </div>
+                </form>
+                """,
             content_type="text/html",
         )
 
@@ -51,15 +87,19 @@ class WeatherBot(Plugin):
             content_type="text/html",
         )
 
-    async def forecast(self, evt, latitude, longitude):
+    async def forecast(
+        self, evt: MessageEvent, latitude: str, longitude: str
+    ) -> Tuple[dict, MediaMessageEventContent]:
         # Scrape Windy multi modal forecast for provided lat/lon
-        # Returns a tuple with the place name and message object with the forecast image
+        # Returns a tuple with the metadata and message object with the forecast image
+        message_id = await self.send_message(evt.room_id, "Launching browser...")
         browser = await launch(
             {
                 "executablePath": "/usr/bin/chromium-browser",
                 "args": ["--no-sandbox", "--headless", "--disable-gpu"],
             }
         )
+        await self.edit_message(evt.room_id, message_id, "Loading page...")
         page = await browser.newPage()
         if self.cookie_account_ss and self.cookie_account_sid:
             await page.setExtraHTTPHeaders(
@@ -82,11 +122,7 @@ class WeatherBot(Plugin):
         """
         )
 
-        logged_in = await page.querySelector(".avatar-wrapper")
-        if logged_in:
-            await evt.respond("Logged in")
-        else:
-            await evt.respond("Not logged in")
+        await self.edit_message(evt.room_id, message_id, "Extracting forecast image...")
 
         forecast = await page.querySelector("iframe")
         if not forecast:
@@ -96,10 +132,13 @@ class WeatherBot(Plugin):
                 "clip": {"x": 0, "y": 120, "height": 800, "width": 1245},
             }
         )
+        logged_in = await page.querySelector(".avatar-wrapper")
         await browser.close()
+        await self.edit_message(evt.room_id, message_id, "Storing forecast image...")
         uri = await self.client.upload_media(forecast_image, mime_type="image/png")
+        await self.client.redact(evt.room_id, message_id)
         return (
-            place,
+            {"location": place, "authenticated": logged_in},
             MediaMessageEventContent(
                 url=uri,
                 body="weather.png",
@@ -113,8 +152,23 @@ class WeatherBot(Plugin):
             ),
         )
 
+    async def weather(self, evt: MessageEvent, lat: str, lon: str):
+        message_id = await self.send_message(evt.room_id, "Fetching weather...")
+        meta, content = await self.forecast(evt, lat, lon)
+        await self.edit_message(
+            evt.room_id,
+            message_id,
+            f"Weather for {meta.get('location')} {'🔒' if meta.get('authenticated') else '🔓'}",
+        )
+        self.log.info(f"Weather found for {meta.get('location')}")
+        if content:
+            await self.client.send_message(evt.room_id, content)
+        else:
+            await self.send_message(evt.room_id, "Forecast did not load.")
+        return
+
     @event.on(EventType.ROOM_MESSAGE)
-    async def handle_message(self, evt) -> None:
+    async def handle_message(self, evt: MessageEvent) -> None:
         if evt.sender == self.client.mxid:
             return
         login_url = f"{self.webapp_url}/login"
@@ -124,49 +178,29 @@ class WeatherBot(Plugin):
         if evt.content.msgtype == MessageType.LOCATION:
             self.log.info(f"Location message recieved from {evt.sender}")
             # Respond to location events with forecast for that location
-            await evt.respond("Fetching weather")
             result = re.search(r"geo:([-\d\.]+),([-\d\.]+)", evt.content.geo_uri)
-            lat = result.group(1)
-            lon = result.group(2)
-            place, content = await self.forecast(evt, lat, lon)
-            await evt.respond(f"Weather for {place}")
-            self.log.info(f"Weather found for {place}")
-            if content:
-                await self.client.send_message(evt.room_id, content)
-            else:
-                await evt.respond(f"Forecast did not load")
-            return
+            return await self.weather(evt, result.group(1), result.group(2))
 
         if evt.content.msgtype == MessageType.TEXT:
             self.log.info(f"Text message recieved from {evt.sender}")
             # Attempt to parse a lat lon out of any other text message
             result = re.search(r"([-\d\.]+) ([-\d\.]+)", evt.content.body)
             if result and result.group(1) and result.group(2):
-                await evt.respond("Fetching weather")
-                lat = result.group(1)
-                lon = result.group(2)
-                self.log.info(f"Parsed lat lon from text message")
-                place, content = await self.forecast(evt, lat, lon)
-                await evt.respond(f"Weather for {place}")
-                self.log.info(f"Weather found for {place}")
-                if content:
-                    await self.client.send_message(evt.room_id, content)
-                else:
-                    await evt.respond(f"Forecast did not load")
-                return
+                return await self.weather(evt, result.group(1), result.group(2))
 
             # Send the version
             if "version" in evt.content.body:
                 self.log.info(f"Version requested")
-                await evt.respond(f"Plugin version {self.loader.meta.version.public}")
-                return
+                return await self.send_message(
+                    evt.room_id, f"Version {self.loader.meta.version.public}"
+                )
 
             if "auth" in evt.content.body:
                 if not self.cookie_account_ss or not self.cookie_account_sid:
-                    await evt.respond(
-                        f"Not logged in. Visit {login_url} to provide windy credentials."
+                    return await self.send_message(
+                        evt.room_id,
+                        f"""No credentials. Visit <a href="{login_url}">the login page</a> to provide windy credentials.""",
                     )
-                    return
                 async with ClientSession(
                     cookies={
                         "_account_sid": self.cookie_account_sid,
@@ -179,13 +213,26 @@ class WeatherBot(Plugin):
                         if resp.status != 200:
                             self.cookie_account_sid = None
                             self.cookie_account_ss = None
-                            await evt.respond(
-                                f"Credentials invalid. Visit {login_url} to provide windy credentials."
+                            return await self.send_message(
+                                evt.room_id,
+                                f"""Credentials invalid. Visit <a href="{login_url}">the login page</a> to provide windy credentials.""",
                             )
-                            return
-                await evt.respond(f"Credentials seem to be okay.")
-                return
+
+                return await self.send_message(evt.room_id, f"""Credentials valid.""")
+
         # Fallback to instructions
-        await evt.respond(
-            f'Either share a location with me, or message me the coordinates you want the weather for: "<lat> <lon>". You can also visit {login_url} to provide windy credentials.'
+        await self.send_message(
+            evt.room_id,
+            f"""<p>This bot will provide you with a weather forecast from <a>windy.com</a>.</p>
+                <p>Available commands:</p>
+                <ul>
+                    <li>Share a location with the bot to get a forecast for that location</li>
+                    <li><code>&lt;lat&gt; &lt;lon&gt;</code> - Get a forecast for these coordinates</li>
+                    <li><code>version</code> - Get the version of the plugin</li>
+                    <li><code>auth</code> - Check if the plugin is logged in to Windy.com</li>
+                </ul>
+                <p>You can also visit <a href="{login_url}">the login page</a> to provide credentials for <a>windy.com</a> which will enable more features (such as 1 hour forecasts) if you have a premium account.
+                </p>""".replace(
+                "\n", ""
+            ),
         )
